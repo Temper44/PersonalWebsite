9af052609a904cfa7fd2baed9fd230568199de88
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2025-03-09 13:27:43 +0100

    add basic floating nav

--- app/components/FloatingNav.tsx
+++ app/components/FloatingNav.tsx
@@ -15,13 +15,13 @@ export default function FloatingNav() {
   return (
     <header className="relative z-10">
       <motion.div
-        className="fixed left-1/2 top-6 h-[3rem] w-[95%] max-w-[330px] rounded-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-sm backdrop-blur-[0.5rem] dark:border-black/40 dark:bg-zinc-950 dark:bg-opacity-75 xs:w-[90%] sm:max-w-[25rem]"
+        className="fixed bottom-6 left-1/2 h-[2.8rem] w-[95%] max-w-[330px] rounded-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-sm backdrop-blur-[0.5rem] dark:border-black/40 dark:bg-zinc-950 dark:bg-opacity-75 xs:w-[90%] sm:h-[3rem] sm:max-w-[25rem]"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
-      <nav className="fixed left-1/2 top-6 flex h-[3rem] -translate-x-1/2 py-2 sm:top-[1.5rem]">
-        <ul className="flex-center gap-1 tracking-wide text-zinc-600 ~text-[0.9rem]/[1rem] sm:gap-4">
+      <nav className="fixed bottom-6 left-1/2 flex h-[2.8rem] -translate-x-1/2 py-2 sm:h-[3rem]">
+        <ul className="flex-center gap-1 tracking-wide text-zinc-600 ~text-[0.875rem]/[1rem] sm:gap-4">
           {sectionLinks.map((link) => (
             <motion.li
               key={link.hash}
@@ -47,7 +47,7 @@ export default function FloatingNav() {
                 {link.name}
                 {link.name === activeSection && (
                   <motion.span
-                    className="absolute inset-0 -z-10 rounded-full border border-black border-opacity-40 dark:border-white dark:border-white/40"
+                    className="absolute inset-0 -z-10 rounded-full bg-zinc-100 dark:bg-zinc-900"
                     layoutId="activeSection"
                     transition={{
                       type: "spring",
