dbcc95d4e7118a1b89bdfd85135ba69106afada8
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2025-03-12 22:15:38 +0100

    fix bug

--- app/components/FixedBackground.tsx
+++ app/components/FixedBackground.tsx
@@ -5,7 +5,7 @@ const FixedBackground = () => {
     <>
       <div className="bg-grain" />
       <div className="flex-center fixed left-0 top-0 z-[-10] h-dvh w-screen bg-dot-black/[0.2] dark:bg-dot-white/[0.1]">
-        <div className="flex-center pointer-events-none fixed inset-0 z-[-10] bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_30%,theme(colors.background))] dark:bg-black" />
+        <div className="flex-center pointer-events-none fixed inset-0 z-[-10] h-dvh bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_30%,theme(colors.background))] dark:bg-black" />
       </div>
     </>
   );
