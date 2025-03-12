0fad4c606c30c975f07a4493023f7142b936c609
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2025-03-12 08:51:47 +0100

    small fixes

--- app/components/ParallaxScrollGallery.tsx
+++ app/components/ParallaxScrollGallery.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import Image from "next/image";
 import { useTransform, useScroll, motion, MotionValue } from "framer-motion";
 // import Lenis from "lenis";
