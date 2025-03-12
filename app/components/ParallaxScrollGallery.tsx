871eb74c4259832934cbce82f777ad548a3a7798
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2025-03-12 08:54:55 +0100

    small fixes

--- app/components/ParallaxScrollGallery.tsx
+++ app/components/ParallaxScrollGallery.tsx
@@ -68,7 +68,7 @@ export default function ParallaxScrollGallery() {
         <Column
           images={[images[2], images[3], images[4]]}
           y={y2}
-          className="top-[-95%]"
+          className="top-[-95%] hidden md:flex"
         />
         <Column
           images={[images[5], images[6], images[7]]}
@@ -101,7 +101,7 @@ const Column = ({ images, y, className }: ColumnProps) => {
   // });
   return (
     <motion.div
-      className={`relative flex h-full w-1/2 flex-col will-change-transform ~gap-3/7 md:w-1/3 lg:w-1/4 ${className}`}
+      className={`relative flex h-full w-full flex-col will-change-transform ~gap-3/7 md:w-1/3 lg:w-1/4 ${className}`}
       style={{ y }}
     >
       {images.map((src, i) => (
