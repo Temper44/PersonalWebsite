c838fbaa7a7a54b971c4e99e10c65d6428f5d1e7
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2025-03-13 08:05:53 +0100

    fix bug

--- app/components/ScrollManager.tsx
+++ app/components/ScrollManager.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useEffect } from "react";
+import { gsap } from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
 export default function ScrollManager() {
   useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
     ScrollTrigger.normalizeScroll(true);
 
     return () => {
