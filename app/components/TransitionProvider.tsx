2ad8bf85d8b9c6da4b56338bf77fff7ac167d0b6
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2025-03-14 23:17:15 +0100

    fix small performance

--- app/components/TransitionProvider.tsx
+++ app/components/TransitionProvider.tsx
@@ -84,11 +84,11 @@ export function TransitionProvider({
 
       <div
         ref={firstLayer}
-        className="fixed inset-0 z-50 translate-y-full overflow-hidden bg-gradient-to-r from-rose-500 to-purple-500 will-change-transform"
+        className="fixed left-0 top-0 z-50 h-[100dvh] w-full translate-y-full overflow-hidden bg-gradient-to-r from-rose-500 to-purple-500 will-change-transform"
       />
       <div
         ref={secondLayer}
-        className="fixed inset-0 z-50 translate-y-full overflow-hidden bg-black will-change-transform"
+        className="fixed left-0 top-0 z-50 h-[100dvh] w-full translate-y-full overflow-hidden bg-black will-change-transform"
       />
     </TransitionRouter>
   );
