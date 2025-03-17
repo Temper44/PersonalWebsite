edc3184369e9ebe1ac34855137879cc42d5da5fc
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2025-03-17 10:00:41 +0100

    change

--- next.config.ts
+++ next.config.ts
@@ -16,4 +16,11 @@ const nextConfig: NextConfig = {
   },
 };
 
+module.exports = {
+  generateBuildId: async () => {
+    // This could be anything, using the latest git hash
+    return process.env.GIT_HASH;
+  },
+};
+
 export default nextConfig;
